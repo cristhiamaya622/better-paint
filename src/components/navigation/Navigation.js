febfa6b7619c8990f2ad5195034c968717231aee
Cristhian Amaya <camaya@hugeinc.com>
2020-12-08 18:48:54 -0500

    Add 'title' to navigation anchor links

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,19 +1,20 @@
 class Navigation {
-  constructor({ logo, shoppingCart }) {
+  constructor({ title, logo, shoppingCart }) {
     this.logo = logo;
     this.shoppingCart = shoppingCart;
+    this.title = title;
   }
 
   render() {
     return `
       <nav class="navigation">
-        <a href="${this.logo.href}">
+        <a href="${this.logo.href}" title="${this.title}">
           <img class="navigation__logo"
                srcset="${this.logo.srcMobile} 767w, ${this.logo.srcDesktop}"
                src="${this.logo.srcMobile}"
                alt="${this.logo.alt}">
         </a>
-        <a href="${this.shoppingCart.href}">
+        <a href="${this.shoppingCart.href}" title="${this.shoppingCart.alt}">
           <img src="${this.shoppingCart.src}" alt="${this.shoppingCart.alt}">
         </a>
       </nav>
