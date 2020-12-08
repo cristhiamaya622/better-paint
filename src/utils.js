ada939c955c841619205f481a86227427530de7f
Cristhian Amaya <camaya@hugeinc.com>
2020-12-08 17:45:29 -0500

    Clean up code and add Puppeteer

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,3 @@
-// exports.getRandomNumber = function() {
-//   return Math.round(Math.random() * 10e10);
-// }
-
-// exports.isValidEmail = function(email) {
-//   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-//   return re.test(String(email).toLowerCase());
-// }
-
 export function getRandomNumber() {
   return Math.round(Math.random() * 10e10);
 }
@@ -15,8 +6,3 @@ export function isValidEmail(email) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
-
-// export default {
-//   getRandomNumber,
-//   isValidEmail
-// }
